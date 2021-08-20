8d3ec439f6f815613193ffda9bae15bbafa6cde1
lzz <1841502391@qq.com>
2021-08-20 10:11:16 +0800

    初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <div>{{ msg }}</div>
+  <Test :propMsg="propMsg" />
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Test from '@/components/Test.vue'
+import { defineComponent, ref } from 'vue'
 
-@Options({
-  components: {
-    HelloWorld,
+export default defineComponent({
+  components: { Test },
+  setup() {
+    const msg = ref('这是首页')
+    const propMsg = '这是prop 信息'
+    return { msg, propMsg }
   },
 })
-export default class Home extends Vue {}
 </script>
